3f1673df46d4097b057f1ead483d28ebdeb466e6
Brandon Le <branndonle@gmail.com>
2025-03-19 14:11:27 -0400

    Documentation Changes/fixes

--- walmart_queries.sql
+++ walmart_queries.sql
@@ -55,7 +55,6 @@ FROM walmart_sales_data_staging;
 
 
 
-
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- --------------------------------------------------------------------------- DATA ENGINEERING -----------------------------------------------------------------------------------------
 
@@ -94,38 +93,60 @@ SELECT *
 FROM walmart_sales_data_staging;
 
 -- -------------------------------------------------------------------------------- GENERAL QUESTIONS -------------------------------------------------------------------------------------
-
--- 1. How many unique cities are there? 
+-- 1. How many cities are there? 
 SELECT DISTINCT city
 FROM walmart_sales_data_staging;
 
 -- 2. Which city is each branch located in?
 SELECT DISTINCT city, branch
 FROM walmart_sales_data_staging;
+
+-- 3. How many sales transactions were there? 
+SELECT SUM(cogs) AS total_cogs FROM walmart_sales_data_staging;  
+
+-- 4. How many products were sold?  
+SELECT SUM(quantity) AS total_products_sold FROM walmart_sales_data_staging;  
+
+-- 5. When was the first and last transaction?
+SELECT MIN(date) AS first_transaction, MAX(date) AS last_transaction FROM walmart_sales_data_staging;  
+
 -- -------------------------------------------------------------------------------- PRODUCT INSIGHT ---------------------------------------------------------------------------------------
--- How many unique product lines does the data have? 
+-- How many product lines are there? 
 SELECT COUNT(DISTINCT product_line) AS unique_products
 FROM walmart_sales_data_staging;
 
--- What is the most common payment method?
+-- What is the most common form of payment?
 SELECT payment_method, COUNT(*) AS counter
 FROM walmart_sales_data_staging
 GROUP BY payment_method
 ORDER BY counter DESC;
 
--- What is the most selling product line?
-SELECT product_line, COUNT(*) AS counter
-FROM walmart_sales_data_staging
-GROUP BY product_line
-ORDER BY counter DESC;
+-- What is the most popular product category?
+SELECT product_line, SUM(quantity) AS total_quantity  
+FROM walmart_sales_data_staging  
+GROUP BY product_line  
+ORDER BY total_quantity DESC;  
 
--- What is the total revenue by month?
+-- Which branches sold more products than the average number of products sold?
+SELECT branch, SUM(quantity) AS sum_quantity
+FROM walmart_sales_data_staging
+GROUP BY branch
+HAVING sum_quantity > AVG(quantity)
+ORDER BY sum_quantity DESC;
+
+-- What is the total revenue each month?
 SELECT month_of_year AS `month` , ROUND(SUM(total), 2) AS total_revenue
 FROM walmart_sales_data_staging
 GROUP BY `month`
 ORDER BY total_revenue DESC;
 
--- What month had the largest COGS?
+-- Which city genereates the most revenue?
+SELECT city, SUM(total) AS total_revenue  
+FROM walmart_sales_data_staging  
+GROUP BY city  
+ORDER BY total_revenue DESC;  
+
+-- What month had the largest Cost of goods sold?
 SELECT month_of_year, SUM(cogs) AS cogs
 FROM walmart_sales_data_staging
 GROUP BY month_of_year
@@ -137,19 +158,13 @@ FROM walmart_sales_data_staging
 GROUP BY product_line
 ORDER BY total_revenue DESC;
 
--- What is the city with the largest revenue?
-SELECT branch, city, SUM(total) AS total_revenue
-FROM walmart_sales_data_staging
-GROUP BY city, branch
-ORDER BY total_revenue DESC;
-
--- What product line had the largest VAT?
-SELECT product_line, AVG(VAT) AS avg_tax
+-- What is the average rating of each product line?
+SELECT product_line, ROUND(AVG(rating), 1) AS avg_rating
 FROM walmart_sales_data_staging
 GROUP BY product_line
-ORDER BY avg_tax DESC;
+ORDER BY avg_rating DESC;
 
--- Label product_lines as "good" or "bad" based on the total if it's greater than average sales
+-- Label product_lines as good or bad based on if the total is greater than average sales
 WITH avg_total AS (
     SELECT AVG(total) AS avg_sales 
     FROM walmart_sales_data_staging
@@ -162,48 +177,46 @@ END AS observation
 FROM walmart_sales_data_staging
 GROUP BY product_line;
 
--- Which branch sold more products than average product sold?
-SELECT branch, SUM(quantity) AS sum_quantity
-FROM walmart_sales_data_staging
-GROUP BY branch
-HAVING sum_quantity > AVG(quantity)
-ORDER BY sum_quantity DESC;
-
--- What is the most common product line by gender?
-SELECT gender, product_line, COUNT(gender) AS gender_cnt
-FROM walmart_sales_data_staging
-GROUP BY gender, product_line
-ORDER BY gender_cnt DESC;
-
--- What is the average rating of each product line?
-SELECT product_line, ROUND(AVG(rating), 1) AS avg_rating
-FROM walmart_sales_data_staging
-GROUP BY product_line
-ORDER BY avg_rating DESC;
-
 SELECT * FROM walmart_sales_data_staging;
+
+-- -------------------------------------------------------------------------------- PROFITABLE & FINANCIAL METRICS -------------------------------------------------------------------------
+-- Which branch is the most profitable?
+SELECT branch, SUM(gross_income) AS total_gross_income  
+FROM walmart_sales_data_staging
+GROUP BY branch  
+ORDER BY total_gross_income DESC;  
+
+-- What is the gross margin percentage by product line?
+SELECT product_line, AVG(gross_margin_percentage) AS avg_gross_margin  
+FROM walmart_sales_data_staging
+GROUP BY product_line  
+ORDER BY avg_gross_margin DESC;  
+
+-- What is the comparison between the cost of goods sold versus revenue?
+SELECT SUM(cogs) AS total_cogs, SUM(total) AS total_revenue,  
+       (SUM(total) - SUM(cogs)) AS profit  
+FROM walmart_sales_data_staging;
 
 -- -------------------------------------------------------------------------------- SALES INSIGHT-----------------------------------------------------------------------------------------
 
--- 1. Number of sales made in each time of the day per weekday
+-- 1. Which city has the largest tax percent?
+SELECT city, AVG(VAT) as avg_vat
+FROM walmart_sales_data_staging
+GROUP BY city
+ORDER BY avg_vat DESC;
+
+-- 2. Number of sales made in each time of the day per weekday
 SELECT time_of_day, COUNT(*) total_sales_per_day
 FROM walmart_sales_data_staging
 WHERE day_of_week IN ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY')
 GROUP BY time_of_day
 ORDER BY total_sales_per_day DESC;
 
--- 2. Which of the customer types brings the most revenue?
+-- 3. Which of the customer types brings the most revenue?
 SELECT customer_type, SUM(total) AS total_revenue
 FROM walmart_sales_data_staging
 GROUP BY customer_type
 ORDER BY total_revenue DESC;
-
--- 3. Which city has the largest tax percent/VAT (Value Added Tax)?
-SELECT city, AVG(VAT) as avg_vat
-FROM walmart_sales_data_staging
-GROUP BY city
-ORDER BY avg_vat DESC;
-
 
 -- 4. Which customer type pays the most in VAT?
 SELECT customer_type, avg(VAT) as avg_vat
@@ -211,86 +224,71 @@ FROM walmart_sales_data_staging
 GROUP BY customer_type
 ORDER BY avg_vat DESC;
 
+-- 5. What are the total sales by each branch? 
+SELECT branch, SUM(total) AS total_sales  
+FROM walmart_sales_data_staging  
+GROUP BY branch  
+ORDER BY total_sales DESC;  
+
+-- 6. What does the sales growth over time look like? 
+ SELECT `date`, SUM(total) AS daily_sales  
+FROM walmart_sales_data_staging  
+GROUP BY `date`  
+ORDER BY `date` ASC;  
+
+-- 7. What is the yearly sales growth?
+SELECT YEAR(date) AS year, SUM(total) AS yearly_sales  
+FROM walmart_sales_data_staging  
+GROUP BY `year`  
+ORDER BY `year`;  
+
 SELECT * FROM walmart_sales_data_staging;
 
 -- -------------------------------------------------------------------------------- CUSTOMER INSIGHT ---------------------------------------------------------------------------------------
 
--- 1. How many unique customer types does the data have?
+-- 1. How many customer types are there?
 SELECT COUNT(DISTINCT customer_type) AS unique_customers
 FROM walmart_sales_data_staging;
 
--- 2. How many unique payment methods does the data have?
-SELECT COUNT(DISTINCT payment_method) AS unique_payment
-FROM walmart_sales_data_staging;
-
--- 3. What is the most common customer type?
-SELECT customer_type, COUNT(*) AS trans_count
-FROM walmart_sales_data_staging
-GROUP BY customer_type; 
-
--- 4. Which customer type makes the most transactions?
+-- 2. Which customer type spends more?
 SELECT customer_type, SUM(total) AS most_transactions
 FROM walmart_sales_data_staging 
 GROUP BY customer_type
 ORDER BY most_transactions DESC;
 
--- 5. What is the gender of most of the customers?
-SELECT DISTINCT gender, COUNT(*) AS gen_cnt
+-- 3. What is the most frequent customer type seen?
+SELECT customer_type, COUNT(*) AS trans_count
 FROM walmart_sales_data_staging
-GROUP BY gender
-ORDER BY gen_cnt DESC;
+GROUP BY customer_type; 
 
--- 6. What is the gender distribution per branch?
-SELECT gender, COUNT(*) AS gender_cnt
-FROM walmart_sales_data_staging
-WHERE branch = 'A'
-GROUP BY gender
-ORDER BY gender DESC;
+-- 4. What is the sales difference between customer segments? 
+SELECT customer_type, SUM(total) AS total_sales, COUNT(invoice_id) AS total_orders  
+FROM walmart_sales_data_staging  
+GROUP BY customer_type;  
 
-SELECT gender, COUNT(*) AS gender_cnt
-FROM walmart_sales_data_staging
-WHERE branch = 'B'
-GROUP BY gender
-ORDER BY gender DESC;
+-- 5. How many payment methods are there?
+SELECT COUNT(DISTINCT payment_method) AS unique_payment
+FROM walmart_sales_data_staging;
 
-SELECT gender, COUNT(*) AS gender_cnt
-FROM walmart_sales_data_staging
-WHERE branch = 'C'
-GROUP BY gender
-ORDER BY gender DESC;
-
--- 7. Which time of the day do customers give most ratings?
+-- 6. When do the most ratings occur during the day? 
 SELECT time_of_day, AVG(rating) AS avg_rating
 FROM walmart_sales_data_staging
 GROUP BY time_of_day
 ORDER BY avg_rating DESC;
 
--- 8. Which time of the day do customers give most ratings per branch?
-SELECT time_of_day, AVG(rating) as avg_rating
-FROM walmart_sales_data_staging
-WHERE branch = 'A'
-GROUP BY time_of_day
-ORDER BY avg_rating DESC;
+-- 7. Which payment method has the Highest average rating? 
+SELECT payment, AVG(rating) AS avg_rating  
+FROM walmart_sales_data_staging  
+GROUP BY payment  
+ORDER BY avg_rating DESC;  
 
-SELECT time_of_day, AVG(rating) as avg_rating
-FROM walmart_sales_data_staging
-WHERE branch = 'B'
-GROUP BY time_of_day
-ORDER BY avg_rating DESC;
-
-SELECT time_of_day, AVG(rating) as avg_rating
-FROM walmart_sales_data_staging
-WHERE branch = 'C'
-GROUP BY time_of_day
-ORDER BY avg_rating DESC;
-
--- 9. Which day of the week has the best avg ratings?
+-- 8. Which day of the week has the best average ratings?
 SELECT day_of_week, AVG(rating) as avg_rating
 FROM walmart_sales_data_staging
 GROUP BY day_of_week
 ORDER BY avg_rating DESC;
 
--- 10. Which day of the week has the best average ratings per branch?
+-- 9. Which day of the week has the best average ratings per branch?
 SELECT day_of_week, AVG(rating) as avg_rating
 FROM walmart_sales_data_staging
 WHERE branch = 'A'
@@ -309,6 +307,11 @@ WHERE branch = 'C'
 GROUP BY day_of_week
 ORDER BY avg_rating DESC;
 
+-- 10. Which branch has the best customer satisfaction rating?
+SELECT branch, AVG(rating) AS avg_rating  
+FROM walmart_sales_data_staging  
+GROUP BY branch  
+ORDER BY avg_rating DESC;  
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- --------------------------------------------------------------------------- END OF EXPLORATORY DATA ANALYSIS ------------------------------------------------------------------------------------
 
